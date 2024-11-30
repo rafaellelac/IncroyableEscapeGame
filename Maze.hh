4c32cb5d971fc5873112db35cb78a1f03f7df90c
saragrissia <sara.grissia@etu.sorbonne-universite.fr>
2024-11-30 16:07:25 +0100

    ajout classes mini jeu

---
+++ Maze.hh
@@ -0,0 +1,18 @@
+
+// cette classe maze hérite de la classe Minigame, elle est donc une classe fille. Cela signifie qu'elle doit implémenter la fonction play() de la classe mère
+//on veut implémenter un labyrinthe
+
+
+class Maze : public Minigame {
+    public:
+    bool play() override {
+        // Implementation of maze game
+        cout << "Playing Maze..." << endl;
+        return true; // Simplified result
+    }
+    void 
+    //le jeu:
+
+
+};
+}
